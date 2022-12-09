180f6086bf262c2caa0eda0593f7bd2cd12515df
garvit Chawla <garvitc8@gmail.com>
2022-12-09 14:53:57 +0530

    add gitignore file

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const Circle=require("./circle");
-const circle=new Circle();
-console.log(circle.area(5));
-console.log(circle.circumference(5));
+//second commit
+const http=require("http");
+const PORT=4000;
+const server=http.createServer((req,res)=>{
+    res.write("Hello Node");
+    res.end();
+});
+
+console.log(`server is running at http://localhost:${PORT}`);
+
+server.listen(PORT);
+
+
+
+// const Circle=require("./circle");
+// const circle=new Circle();
+// console.log(circle.area(5));
+// console.log(circle.circumference(5));
 // console.log(area(5));
 // console.log(circumference(5));
 
 
-//second commit
-// const http=require("http");
-// const server=http.createServer((req,res)=>{
-//     res.write("Hello Node");
-//     res.end();
-// });
 
-
-// server.listen(3000);
