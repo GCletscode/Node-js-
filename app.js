24bb7d424fa187d1f3e4ce5053324a3b31abee21
garvit Chawla <garvitc8@gmail.com>
2022-12-10 16:48:25 +0530

    append file

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//second commit
 const http=require("http");
 const fs=require("fs");
 const { homedir } = require("os");
@@ -14,13 +13,31 @@ const server=http.createServer((req,res)=>{
     }
     else if(req.url=="/about"){
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write("<h1>I am on about page</h1>");
-        res.end();
+        fs.readFile("temp/test.html","utf8",(err,data)=>{
+            if(err) throw err;
+            res.write(data);
+            res.end();
+        })
+    }
+    else if(req.url=="/create-file"){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        for(let i=0;i<100000;i++){
+             fs.appendFile("temp/test.html","<h1>Love to do node js</h1>",(err)=>{
+            if(err) throw err;
+        });
+    }
+            res.write("file has been created");
+            res.end(); 
+        
     }
     else{
     res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.write("<h1>page not found</h1>");  
-    res.end();  
+    fs.readFile("page/404.html","utf8",(err,data)=>{
+        if(err) throw err;
+        res.write(data);
+        res.end(); 
+    }) 
+     
     }
     
     
@@ -33,12 +50,6 @@ server.listen(PORT);
 
 
 
-// const Circle=require("./circle");
-// const circle=new Circle();
-// console.log(circle.area(5));
-// console.log(circle.circumference(5));
-// console.log(area(5));
-// console.log(circumference(5));
 
 
 
