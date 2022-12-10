c6f202a259f426efd83e9981bcd095ad806180a3
garvit Chawla <garvitc8@gmail.com>
2022-12-10 16:07:15 +0530

    file system commit

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 //second commit
 const http=require("http");
+const fs=require("fs");
+const { homedir } = require("os");
 const PORT=4000;
 const server=http.createServer((req,res)=>{
-    res.write("Hello Node");
-    res.end();
+    if(req.url=="/"){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile("page/home.html","utf8",(err,data)=>{
+            if(err) throw err;
+            res.write(data);
+            res.end();
+        });  
+    }
+    else if(req.url=="/about"){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write("<h1>I am on about page</h1>");
+        res.end();
+    }
+    else{
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write("<h1>page not found</h1>");  
+    res.end();  
+    }
+    
+    
 });
+
 
 console.log(`server is running at http://localhost:${PORT}`);
 
