6f4b056d0f3528f89d1be607775fea8d72e65ba6
garvit Chawla <garvitc8@gmail.com>
2022-12-13 20:43:40 +0530

    ext file in express

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express=require("express");
+const path=require("path");
 const bookRouter=require("./bookRouter");
 const router=express.Router();
 
 
 
-//request endpoints
 
+//request endpoints
+router.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/../page/home.html"));
+});
 router.use("/book",bookRouter);
 
 router.all("/*",(req,res)=>{
